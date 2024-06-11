153b42dc3a689206f4898c51fee3b4ca972390b2
Jonathon Rivers <jdr2045@gmail.com>
2024-06-10 20:41:50 -0400

    Prisma init

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import swagger from "@elysiajs/swagger";
 import Elysia from "elysia";
+import { prisma } from "./prisma";
 
 export const app = new Elysia().use(swagger());
+
+app.group("/users", (router) =>
+  router.get("/", async () => await prisma.user.findMany())
+);
