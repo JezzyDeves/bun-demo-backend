3e9eafbcf81042a191a05e1d1318750f0d541843
Jonathon Rivers <jdr2045@gmail.com>
2024-06-12 19:13:06 -0400

    Added signUp endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,46 @@
+import jwt from "@elysiajs/jwt";
 import swagger from "@elysiajs/swagger";
-import Elysia from "elysia";
+import { User } from "@prisma/client";
+import Elysia, { t } from "elysia";
 import { prisma } from "./prisma";
 
-export const app = new Elysia().use(swagger());
+export const app = new Elysia().use(swagger()).use(
+  jwt({
+    secret: "Secret Key",
+  })
+);
 
-app.group("/users", (router) =>
-  router.get("/", async () => await prisma.user.findMany())
+app.group("/auth", (router) =>
+  router.post(
+    "/signUp",
+    async ({ body, jwt, cookie: { auth } }) => {
+      const existingUser = await prisma.user.findFirst({
+        where: { username: body.username },
+      });
+
+      if (existingUser) {
+        return new Response("User already exists", { status: 400 });
+      }
+
+      const passwordHash = await Bun.password.hash(body.password);
+
+      await prisma.user.create({
+        data: { ...body, password: passwordHash },
+      });
+
+      auth.set({
+        value: jwt.sign({ username: body.username }),
+      });
+
+      return "Signed up successfully";
+    },
+    {
+      body: t.Object({
+        firstName: t.String({ minLength: 1 }),
+        lastName: t.String({ minLength: 1 }),
+        password: t.String({ minLength: 1 }),
+        username: t.String({ minLength: 1 }),
+      }),
+    }
+  )
 );
